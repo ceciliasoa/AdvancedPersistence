8464f5bc38908c2be14fb2a12faae12560fd6246
Cecilia <ceciliasoares0@gmail.com>
2020-10-16 17:06:07 -0300

    saving data and add new name

---
+++ coreDataCloudKit/Core Data/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  coreDataCloudKit
+//
+//  Created by Cecilia Soares on 14/10/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
